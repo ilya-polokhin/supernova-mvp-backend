d863074a9691061a6605f5824815a650de63e373
ilya-polokhin <ilya.polokhin@gmail.com>
2026-02-26 16:39:36 -0500

    Update session.js

--- api/session.js
+++ api/session.js
@@ -1,6 +1,13 @@
 import OpenAI from "openai";
 
 export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
